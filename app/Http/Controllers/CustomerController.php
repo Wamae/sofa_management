09027d3a948c2cd9b19c3aac121f9f1e1fb3bf99
Wamae <wamaebenson06@gmail.com>
2018-06-04 12:56:14 +0300

    Customers: Email fix

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -43,16 +43,21 @@ class CustomerController extends Controller
             'last_name' => 'required',
             'phone' => 'required',
             'account_id' => 'required|int',
-            'created_by'=>'required|int'
+            'created_by' => 'required|int'
         ]);
 
         if ($validator->passes()) {
             $pin = rand(1000, 9999);
+
             $customer = new Customer();
             $customer->first_name = ucwords(strtolower($request->first_name));
             $customer->last_name = ucwords(strtolower($request->last_name));
             $customer->phone = $request->phone;
-            $customer->email = strtolower($request->email);
+            $email = $request->email;
+            if(strlen(trim($email)) > 0){
+                $customer->email = strtolower($email);
+            }
+
             $customer->address = ucwords(strtolower($request->address));
             $customer->password = Hash::make($pin);
             $customer->account_id = $request->account_id;
@@ -60,11 +65,11 @@ class CustomerController extends Controller
 
             $result = $customer->save();
 
-            if($result){
+            if ($result) {
                 //TODO::Send SMS notification
                 return array(
-                    'status'=>1,
-                    'message'=>"Customer successfully created"
+                    'status' => 1,
+                    'message' => "Customer successfully created"
                 );
             }
 
@@ -85,7 +90,7 @@ class CustomerController extends Controller
      */
     public function show($id)
     {
-        $customer = Customer::find($id)->select(['id','first_name','last_name','phone','email','address'])->get();
+        $customer = Customer::find($id)->select(['id', 'first_name', 'last_name', 'phone', 'email', 'address'])->get();
 
         return $customer;
     }
@@ -115,7 +120,7 @@ class CustomerController extends Controller
             'last_name' => 'required',
             'phone' => 'required',
             'account_id' => 'required|int',
-            'updated_by'=>'required|int'
+            'updated_by' => 'required|int'
         ]);
 
         if ($validator->passes()) {
@@ -130,10 +135,10 @@ class CustomerController extends Controller
 
             $result = $customer->save();
 
-            if($result){
+            if ($result) {
                 return array(
-                    'status'=>1,
-                    'message'=>"Customer successfully updated"
+                    'status' => 1,
+                    'message' => "Customer successfully updated"
                 );
             }
 
@@ -164,15 +169,15 @@ class CustomerController extends Controller
     public function getAllCustomers(Request $request)
     {
         $accountId = $request->account_id;
-        $customers = Customer::select(['id','first_name','last_name','phone','email','address'])->where('account_id','=',$accountId)->get();
+        $customers = Customer::select(['id', 'first_name', 'last_name', 'phone', 'email', 'address'])->where('account_id', '=', $accountId)->get();
 
-        if($customers->count() > 0){
+        if ($customers->count() > 0) {
             return $customers;
-        }else{
+        } else {
             return array(
-                'status'=>1,
-                'message'=>"No customers created yet!",
-                "data"=>null
+                'status' => 1,
+                'message' => "No customers created yet!",
+                "data" => null
             );
         }
     }
